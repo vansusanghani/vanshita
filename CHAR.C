b7e5ccb5a39b71fd704445e0da2193c67bae0bd4
dizzaharkhani <dizzaharkhani1106@gmail.com>
2023-05-16 10:35:54 +0530

    Initial commit

---
+++ CHAR.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+	char a;
+	clrscr();
+
+	printf("please enter one character");
+	scanf("%c",&a);
+	printf(                                                                                        "%c",a);
+	getch();
+}
